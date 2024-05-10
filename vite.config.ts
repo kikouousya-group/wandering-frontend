0a33a42827c5436d702e9ca53af849d56023da35
kikouousya <kikouousya@gmail.com>
2024-05-10 22:08:44 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^~/,
+        replacement: '',
+      },
+      // {
+      //   find: '@',
+      //   replacement: path.resolve(__dirname, './src')
+      // }
+    ],
+  }
+
 })
